49d77e71a4b13fbf8bef68bc44b799f78b6b2a6a
DylDev <dylanb92010@gmail.com>
2025-09-03 21:10:03 -0400

    Set up base constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -3,17 +3,62 @@ package org.firstinspires.ftc.teamcode.pedroPathing;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
+public class Constants {
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(15) // Weight of the robot in kg (this is estimated since we dont have a scale)
+            .forwardZeroPowerAcceleration(0) // TUNE THIS
+            .lateralZeroPowerAcceleration(0) // TUNE THIS
+            // Use dual PIDs for accuracy
+            .useSecondaryTranslationalPIDF(true)
+            .useSecondaryHeadingPIDF(true)
+            .useSecondaryDrivePIDF(true);
+
+    // Probably shouldn't mess with this for now
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1) // This should pretty much always stay at 1 (100%)
+            // Names of the motors in the hardware map (these are the defaults from the FTC SDK examples)
+            .rightFrontMotorName("front_right_drive")
+            .rightRearMotorName("back_right_drive")
+            .leftRearMotorName("back_left_drive")
+            .leftFrontMotorName("front_left_drive")
+            // Adjust the motor directions according to your robot
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .xVelocity(0) // TUNE THIS
+            .yVelocity(0); // TUNE THIS
+
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+            // Adjust the pod distances according to this diagram:
+            // https://pedropathing.com/_next/static/media/odometry.528df183.png
+            .forwardPodY(0)
+            .strafePodX(0)
+            .distanceUnit(DistanceUnit.INCH) // Measure everything in this unit (INCH or MM)
+            .hardwareMapName("pinpoint") // Hardware map name of the pinpoint
+            // Change this to goBILDA_4_BAR_POD or goBILDA_SWINGARM_POD
+            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
+            // Change this to FORWARD or REVERSE according to the pod direction
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSE)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
+
+    // Don't touch this
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
                 .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(driveConstants)
+                .pinpointLocalizer(localizerConstants)
                 .build();
     }
 }
